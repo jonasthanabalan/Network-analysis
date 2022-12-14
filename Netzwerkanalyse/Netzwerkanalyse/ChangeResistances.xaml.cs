f9bad4d795b7df40ac9d6367e3e374b0a5b35f5a
jonasthanabalan <112861628+jonasthanabalan@users.noreply.github.com>
2022-12-14 11:31:47 +0100

    Add files via upload

--- Netzwerkanalyse/Netzwerkanalyse/ChangeResistances.xaml.cs
+++ Netzwerkanalyse/Netzwerkanalyse/ChangeResistances.xaml.cs
@@ -23,5 +23,31 @@ namespace Netzwerkanalyse
         {
             InitializeComponent();
         }
+
+        public double[] getInput()
+        {
+            double[] input = new double[5];
+            try
+            {
+                input[0] = Convert.ToDouble(R1.Text);
+                input[1] = Convert.ToDouble(R2.Text);
+                input[2] = Convert.ToDouble(R3.Text);
+                input[3] = Convert.ToDouble(R5.Text);
+                input[4] = Convert.ToDouble(R6.Text);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return input;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Resistances resistances = new Resistances();
+            resistances.write(getInput());
+            Close();
+        }
     }
 }
--- Netzwerkanalyse/Netzwerkanalyse/Resistances.cs
+++ Netzwerkanalyse/Netzwerkanalyse/Resistances.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,15 @@ namespace Netzwerkanalyse
 {
     internal class Resistances
     {
-        public void write()
+        public void write(double[] resistances)
         {
-
+            using (var sr = new StreamWriter("resistances.dat"))
+            {
+                foreach (int value in resistances)
+                {
+                    sr.Write(value + ", ");
+                }
+            }
         }
     }
 }
